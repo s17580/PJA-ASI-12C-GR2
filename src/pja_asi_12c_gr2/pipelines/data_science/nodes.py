d5f1ef219171f743b8d07c4509048a2c03c6b94f
AdamKaczkowski <s23020@pjwstk.edu.pl>
2024-04-19 17:26:00 +0200

    Kedro Update

--- src/pja_asi_12c_gr2/pipelines/data_science/nodes.py
+++ src/pja_asi_12c_gr2/pipelines/data_science/nodes.py
@@ -1,15 +1,9 @@
-import logging
-from typing import Dict, Tuple
-
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import max_error, mean_absolute_error, r2_score
+from kedro.io import DataCatalog
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import (
     accuracy_score,
     precision_score,
@@ -17,12 +11,10 @@ from sklearn.metrics import (
     f1_score,
     confusion_matrix,
 )
-import pickle
-from sklearn.base import is_classifier
 
 
 def transform_data(pokemons):
-    # df = load_data()
+
     pokemons["Type 2"] = pokemons["Type 2"].fillna("None")  # Filling missing values
 
     # Features and Labels
@@ -53,32 +45,36 @@ def transform_data(pokemons):
     )
 
     # Splitting the dataset into training and testing sets
-    x_train1, x_test1, y_train1, y_test1 = train_test_split(
+    x_train, x_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, stratify=y, random_state=0
     )
-    return x_train1, x_test1, y_train1, y_test1, clf
+    return x_train, x_test, y_train, y_test, clf
 
 
-def machine_learning(x_train1, x_test1, y_train1, y_test1, clf):
-    clf.fit(x_train1, y_train1)
+def machine_learning(x_train, x_test, y_train, y_test, clf):
+    clf.fit(x_train, y_train)
     return clf
 
 
-# def evaluate_model1(x_test1, y_test1, classifier):
-#         y_pred = classifier.predict(x_test1)
+def evaluate_model(x_test, y_test, classifier):
+    y_pred = classifier.predict(x_test)
 
-#         accuracy = accuracy_score(y_test1, y_pred)
-#         precision = precision_score(y_test1, y_pred, average="binary")
-#         recall = recall_score(y_test1, y_pred, average="binary")
-#         f1 = f1_score(y_test1, y_pred, average="binary")
-#         conf_matrix = confusion_matrix(y_test1, y_pred)
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred, average="binary")
+    recall = recall_score(y_test, y_pred, average="binary")
+    f1 = f1_score(y_test, y_pred, average="binary")
+    conf_matrix = confusion_matrix(y_test, y_pred)
 
-#         print("Accuracy:", accuracy)
-#         print("Precision:", precision)
-#         print("Recall:", recall)
-#         print("F1 Score:", f1)
-#         print("Confusion Matrix:\n", conf_matrix)
+    evaluation_results = {
+        "accuracy": accuracy,
+        "precision": precision,
+        "recall": recall,
+        "f1_score": f1,
+        "confusion_matrix": conf_matrix.tolist(),  # konwersja na listę dla obsługi JSON
+    }
 
-# def release_model(pokemons):
-#     with open("pokemon_classifier.pkl", "wb") as f:
-#         pickle.dump((transform_data, is_classifier), f)
+    return evaluation_results
+
+
+def release_model(catalog: DataCatalog, evaluation_results: dict, classifier):
+    catalog.save("evaluation_results", evaluation_results)
--- src/pja_asi_12c_gr2/pipelines/data_science/pipeline.py
+++ src/pja_asi_12c_gr2/pipelines/data_science/pipeline.py
@@ -3,6 +3,7 @@ from kedro.pipeline import Pipeline, node, pipeline
 from .nodes import (
     transform_data,
     machine_learning,
+    evaluate_model,
 )
 
 
@@ -12,18 +13,19 @@ def create_pipeline(**kwargs) -> Pipeline:
             node(
                 func=transform_data,
                 inputs="pokemons",
-                outputs=["x_train1", "x_test1", "y_train1", "y_test1", "clf"],
+                outputs=["x_train", "x_test", "y_train", "y_test", "clf"],
                 name="transform_data_node",
             ),
             node(
                 func=machine_learning,
-                inputs=["x_train1", "x_test1", "y_train1", "y_test1", "clf"],
-                outputs="machine_learning_node",
+                inputs=["x_train", "x_test", "y_train", "y_test", "clf"],
+                outputs="classifier",
+                name="machine_learning_node",
             ),
-            # node(
-            #     func=evaluate_model1,
-            #     inputs=["x_test1", "y_test1", "classifier"],
-            #     outputs="metrics1",
-            #     ),
+            node(
+                func=evaluate_model,
+                inputs=["x_test", "y_test", "classifier"],
+                outputs="release_model",
+            ),
         ]
     )
